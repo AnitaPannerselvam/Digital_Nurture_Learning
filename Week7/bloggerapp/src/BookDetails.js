db27306e24140342bd1e94866b0b1ee352d48bab
AnitaPannerselvam <125266023+AnitaPannerselvam@users.noreply.github.com>
2025-08-04 15:01:44 +0530

    Handson-5th Exercise
    Handson-5th Exercise questions are done and answers are uploaded.

---
+++ Week7/bloggerapp/src/BookDetails.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+function BookDetails({ books }) {
+  return (
+    <div>
+      <h2>📚 Book List</h2>
+      <ul>
+        {books.map((book, index) => (
+          <li key={book.id}>
+            <strong>{book.title}</strong> by {book.author}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default BookDetails;
