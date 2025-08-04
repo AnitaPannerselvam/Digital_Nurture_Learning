eab057492a6d17984d4c6fa8993aba89bcdfa647
AnitaPannerselvam <125266023+AnitaPannerselvam@users.noreply.github.com>
2025-08-04 15:05:51 +0530

    Handson-5th & 6th Exercise
    Handson-5th & 6th Exercise questions and answers are uploaded

---
+++ Week7/bloggerapp/src/CourseDetails.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+function CourseDetails() {
+  return (
+    <div>
+      <h2>🎓 Available Courses</h2>
+      <ul>
+        <li>React Fundamentals</li>
+        <li>Advanced JavaScript</li>
+        <li>Web Accessibility</li>
+      </ul>
+    </div>
+  );
+}
+
+export default CourseDetails;
