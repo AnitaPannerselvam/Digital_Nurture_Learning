b96599266b3b567345ee4e5add84cc1857a654fe
AnitaPannerselvam <125266023+AnitaPannerselvam@users.noreply.github.com>
2025-08-04 14:59:28 +0530

    Handson-7th Exercise

---
+++ Week7/bloggerapp/src/BlogDetails.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+function BlogDetails() {
+  return (
+    <div>
+      <h2>📝 Blog Highlights</h2>
+      <p>1. How to Learn React</p>
+      <p>2. State vs Props Explained</p>
+    </div>
+  );
+}
+
+export default BlogDetails;
